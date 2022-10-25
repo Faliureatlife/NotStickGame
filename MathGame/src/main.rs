7cda3f5fceea387ad1b4e0f0f12abd7c217ffdd1
Faliureatlife <38265987+Faliureatlife@users.noreply.github.com>
2022-10-25 13:09:37 -0700

    Comment and slices

--- MathGame/src/main.rs
+++ MathGame/src/main.rs
@@ -28,18 +28,31 @@ fn main() -> Result<(), pixels::Error> {
     let mut pixels = Pixels::new(size.width, size.height, surface_texture)?;
 
     //Have mutable frame buffer
-    let frame = pixels.get_frame();
+    let frame = pixels.get_frame(); // returns slice called pixels
+    //pixel is the iterator variable
+    //frame is the slice
+    //chunks_exact_mut allows muting group in slice, 4 is the aomunt that will be changed at once
     for pixel in frame.chunks_exact_mut(4) {
         pixel[0] = 0x55; // R
         pixel[1] = 0x77; // G
         pixel[2] = 0xFF; // B
         pixel[3] = 0x11; // A
     }
+    let fr = pixels.get_frame().split_mut(|x| x % 240 == 0);
+    for pixel in fr.chunks_exact_mut(4) {
+        pixel[0] = 0x01; // R
+        pixel[1] = 0x01; // G
+        pixel[2] = 0x01; // B
+        pixel[3] = 0x11; // A
+    }
+
+
+
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
         pixels.render().unwrap();
-
+        //close window
         match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
