2c292ca9afbc79c5e1f733414e207a70d98af3b0
Faliureatlife <38265987+Faliureatlife@users.noreply.github.com>
2022-11-04 09:28:53 -0700

    Out of bounds error after 303k places

--- math_game/src/main.rs
+++ math_game/src/main.rs
@@ -1,6 +1,5 @@
 //extra functions for idiomadic code or wtv
 mod render;
-
 use pixels::Pixels;
 //Dont just import all of pixels at some point
 // use pixels::wgpu::Color;
@@ -13,6 +12,7 @@ use winit::{
 };
  use std::{
      env,
+     u8,
 //     fs::*,
 //     time::Duration,
 //     thread::sleep,
@@ -23,7 +23,7 @@ fn main() -> Result<(), pixels::Error> {
     //where event loop is created for future event_loop.run
     let event_loop = EventLoop::new();
     let var = "RUST_BACKTRACE";
-    env::set_var(var, "1");
+    env::set_var(var, "full");
     //Create window and give it Logical Size of 720 4:3
     let window = Window::new(&event_loop).unwrap();
     window.set_inner_size(LogicalSize::new(720, 540));
@@ -37,26 +37,26 @@ fn main() -> Result<(), pixels::Error> {
 
     let screen = Screen::new("WorldData/houses.txt");
 
+    let pix = pixels.get_frame();
+    screen.draw(pix);
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-        let pix = pixels.get_frame();
-        screen.draw(pix);
-        {if pixels
-        .render()
-        .map_err(|e| panic!("pixels.render() failed: {}", e))
-        .is_err()
-        {}}
-        //close window
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            _ => (),
-        }
-    });
-    //Ok(())
+    // event_loop.run(move |event, _, control_flow| {
+    //     *control_flow = ControlFlow::Wait;
+    //     {if pixels
+    //     .render()
+    //     .map_err(|e| panic!("pixels.render() failed: {}", e))
+    //     .is_err()
+    //     {}}
+    //     //close window
+    //     match event {
+    //         Event::WindowEvent {
+    //             event: WindowEvent::CloseRequested,
+    //             window_id,
+    //         } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+    //         _ => (),
+    //     }
+    // });
+    Ok(())
     //use to crash program safely
     //
 }
@@ -94,26 +94,23 @@ impl Screen {
             // let a = self.area.capacity();
             // println!("{}",a);
 
-            println!("pixel is {:?}",pixel);
+            //println!("pixel is {:?}",pixel);
             let r: Vec<u8> = vec![self.area[pos], self.area[pos+1]];
-            println!("r is {:?}",r);
+            //println!("r is {:?}",r);
             let red = std::str::from_utf8(&r).unwrap();
-            println!("red is {:?}",red);
+            //println!("red is {:?}",red);
             let g: Vec<u8> = vec![self.area[pos+2], self.area[pos+3]];
-            println!("g is {:?}",g);
+            //println!("g is {:?}",g);
             let green = std::str::from_utf8(&g).unwrap();
-            println!("green is {:?}",green);
+            //println!("green is {:?}",green);
             let b: Vec<u8> = vec![self.area[pos+4], self.area[pos+5]];
-            println!("b is {:?}",b);
+            //println!("b is {:?}",b);
             let blue = std::str::from_utf8(&b).unwrap();
-            println!("blue is {:?}",blue);
-
-            let asd = FromHex(red);
-
+            //println!("blue is {:?}",blue);
             //Shoves string pointer into u8 sized hole
-            pixel[0] = red.parse().unwrap(); // R
-            pixel[1] = green.parse().unwrap(); // G
-            pixel[2] = blue.parse().unwrap(); // B
+            pixel[0] = u8::from_str_radix(red,16).unwrap(); // R
+            pixel[1] = u8::from_str_radix(green,16).unwrap(); // G
+            pixel[2] = u8::from_str_radix(blue,16).unwrap(); // B
             pixel[3] = 0xFF; // A
             it += 1;
         }
