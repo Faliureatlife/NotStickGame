bb5a5146ffb68431e7b5dd0990974f282536b32a
bob <mnick900@gmail.com>
2022-11-27 11:47:46 -0800

    move commands and partway through timing checks

--- math_game/src/main.rs
+++ math_game/src/main.rs
@@ -3,6 +3,14 @@ mod render;
 use pixels::Pixels;
 //Dont just import all of pixels at some point
 // use pixels::wgpu::Color;
+use std::{
+    env,
+    time::SystemTime,
+    //     fs::*,
+    //     time::Duration,
+    //     thread::sleep,
+    u8,
+};
 use winit::{
     dpi::PhysicalSize,
     //dpi::PhysicalSize,
@@ -11,21 +19,12 @@ use winit::{
     window::Window,
 };
 use winit_input_helper::WinitInputHelper;
-use std::{
-     env,
-     u8,
-     time::SystemTime,
-//     fs::*,
-//     time::Duration,
-//     thread::sleep,
- };
 
- //starting position of player
- const START_Y: u16 = 0;
- const START_X: u16 = 0;
+//starting position of player
+const START_Y: u16 = 0;
+const START_X: u16 = 0;
 
 fn main() -> Result<(), pixels::Error> {
-    let mut times:SystemTime = SystemTime::now();
     //where event loop is created for the future event_loop.run
     let event_loop = EventLoop::new();
 
@@ -59,9 +58,9 @@ fn main() -> Result<(), pixels::Error> {
             //do the thinking for the drawing process
             //render the frame buffer and panic if it has something passed to it
             if pixels
-            .render()
-            .map_err(|e| panic!("pixels.render() failed: {}",e))
-            .is_err()
+                .render()
+                .map_err(|e| panic!("pixels.render() failed: {}", e))
+                .is_err()
             {
                 //after the panic close the process
                 *control_flow = ControlFlow::Exit;
@@ -70,21 +69,30 @@ fn main() -> Result<(), pixels::Error> {
         }
         //update part of code that handles keypresses and simple window things
         if input.update(&event) {
+            //make into a match statement at some point maybe
             //close on pressing esc
             if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
                 *control_flow = ControlFlow::Exit;
                 return;
             }
-            if input.key_pressed(VirtualKeyCode::S){
-                screen.player.y_pos += 1;
+            if input.key_pressed(VirtualKeyCode::W) {
+                screen.player.mov(1);
+                return;
+            }
+            if input.key_pressed(VirtualKeyCode::A) {
+                screen.player.mov(2);
+                return;
+            }
+            if input.key_pressed(VirtualKeyCode::S) {
+                screen.player.mov(3);
+                return;
+            }
+            if input.key_pressed(VirtualKeyCode::D) {
+                screen.player.mov(4);
                 return;
             }
             //after updates happen redraw the screen
-            let time_n = SystemTime::now();
-            let diff = time_n.duration_since(times).unwrap();
-            println!("{:.?}",diff);
             window.request_redraw();
-            times = SystemTime::now();
         };
     });
     //Ok(())
@@ -107,10 +115,28 @@ impl Player {
             sprite: std::fs::read(spr).unwrap(),
         }
     }
+    fn mov(&mut self, dir: u8) {
+        match dir {
+            //Move up W
+            1 if self.y_pos > 1 => self.y_pos -= 2,
+            1 => {}
+            //Move left A
+            2 if self.x_pos > 1 => self.x_pos -= 2,
+            2 => {}
+            //Move down S
+            3 if self.y_pos < 539 => self.y_pos += 2,
+            3 => {}
+            //Move right D
+            4 if self.x_pos < 719 => self.x_pos += 2,
+            4 => {}
+            _ => panic!("Invalid movement"),
+        }
+    }
 }
 
 struct Screen {
     player: Player,
+    //player_pos: enum
     //triggers: idk
     //baddies: Vec<Baddie>,
     area: Vec<u8>,
@@ -126,7 +152,7 @@ impl Screen {
         }
     }
 
-    fn draw(&self, pix: &mut [u8]){
+    fn draw(&self, pix: &mut [u8]) {
         //iterator var
         let mut fb = self.area.clone();
         //fb.get_mut(((720*self.player.x_pos + self.player.y_pos) as usize)..(((720*self.player.x_pos + self.player.y_pos) as usize )+self.player.sprite.len())) = &self.player.sprite;
@@ -136,34 +162,54 @@ impl Screen {
         //^^ failed ideas that im keeping because they could be useful
 
         //below here needs to be recommented
-                        // let (b4,l8) = fb.split_at_mut((720*self.player.x_pos + self.player.y_pos) as usize);
-                        // let (_,l8r) = l8.split_at_mut((720*self.player.x_pos + self.player.y_pos) as usize + self.player.sprite.len());
-                        // let good = &mut self.player.sprite
-                        // .as_slice()
-                        // .to_owned();
-                        // fb = [b4,good,l8r].concat();
+        // let (b4,l8) = fb.split_at_mut((720*self.player.x_pos + self.player.y_pos) as usize);
+        // let (_,l8r) = l8.split_at_mut((720*self.player.x_pos + self.player.y_pos) as usize + self.player.sprite.len());
+        // let good = &mut self.player.sprite
+        // .as_slice()
+        // .to_owned();
+        // fb = [b4,good,l8r].concat();
 
         //entities are 18x27
-        for v in 0..27{   //0-26
+        //whole thing takes about 8ms
+        //each iteration is about 300 microseconds
+        for v in 0..27 {
+            //0-26
             //println!("{}",v);
-            let (b4,l8) = fb.split_at((((6*720*self.player.y_pos)+ 6*(self.player.x_pos)) as u32 +(6*720*v)) as usize);
-            //println!{"the split point is {}",p}
-            let (_,l8r) = l8.split_at(108 as usize);
-            //THIS LINE IS NOT THE PROBLEM
-            //jk lol it is
-            let good = &(self.player.sprite.get((6*18*v) as usize..(6*18*v + 18*6) as usize).unwrap());
-            fb = [b4,good,l8r].concat();
-//test infodumps
-//println!("the first part is {}, and the second is {}, and this is equal to {}",b4.len(),l8.len(),b4.len() + l8.len());
-//println!("the discrepancy of the first part is {} \n",(b4.len() + l8.len())-(a.len() + l8r.len()));
-        }
-//framebuffer dump for errors
-//std::fs::write("asdf", &fb).unwrap();
+            //about 130 nanoseconds
+            let (b4, l8) = fb.split_at(
+                (((6 * 720 * self.player.y_pos) + 6 * (self.player.x_pos)) as u32 + (6 * 720 * v))
+                as usize,
+            );
 
+            //println!{"the split point is {}",p}
+            //either 70 or 200 nanoseconds
+            let (_, l8r) = l8.split_at(108);
+
+            //about 100 nanoseconds again with 400 spikes
+            let good = &(self
+                .player
+                .sprite
+                .get((6 * 18 * v) as usize..(6 * 18 * v + 18 * 6) as usize)
+                .unwrap());
+            let times = SystemTime::now();
+
+            fb = [b4, good, l8r].concat();
+
+            let time_n = SystemTime::now();
+            let diff = time_n.duration_since(times).unwrap();
+            println!("{:.?}", diff);
+
+            //test infodumps
+            //println!("the first part is {}, and the second is {}, and this is equal to {}",b4.len(),l8.len(),b4.len() + l8.len());
+            //println!("the discrepancy of the first part is {} \n",(b4.len() + l8.len())-(a.len() + l8r.len()));
+        }
+        //until this point is about 8ms, too long id say
+        //framebuffer dump for errors
+        //std::fs::write("asdf", &fb).unwrap();
 
         for (it, pixel) in pix.chunks_exact_mut(4).enumerate() {
             //i*6 is the byte chunk
-            let pos = it*6;
+            let pos = it * 6;
             //i hate this part
             // let a = self.area.capacity();
             // println!("{}",a);
@@ -173,32 +219,38 @@ impl Screen {
 
             //kinda hacky workaround to turn two &str into a valid hex byte
             //takes two u8 and puts together
-            let r: Vec<u8> = vec![fb[pos], fb[pos+1]];
+            let r: Vec<u8> = vec![fb[pos], fb[pos + 1]];
             //takes it from u8 bytes to &str UTF-8
             let red = std::str::from_utf8(&r).unwrap();
             //sets red value in the thing into the hex value contained in red
-            pixel[0] =  u8::from_str_radix(red,16).unwrap(); // R
+            pixel[0] = u8::from_str_radix(red, 16).unwrap(); // R
 
-            let g: Vec<u8> = vec![fb[pos+2], fb[pos+3]];
+            let g: Vec<u8> = vec![fb[pos + 2], fb[pos + 3]];
             let green = std::str::from_utf8(&g).unwrap();
-            pixel[1] = u8::from_str_radix(green,16).unwrap(); // G
+            pixel[1] = u8::from_str_radix(green, 16).unwrap(); // G
 
-            let b: Vec<u8> = vec![fb[pos+4], fb[pos+5]];
+            let b: Vec<u8> = vec![fb[pos + 4], fb[pos + 5]];
             let blue = std::str::from_utf8(&b).unwrap();
-            pixel[2] = u8::from_str_radix(blue,16).unwrap(); // B
-            //Sets transparency value to none because that is stupid
+            pixel[2] = u8::from_str_radix(blue, 16).unwrap(); // B
+                                                              //Sets transparency value to none because that is stupid
             pixel[3] = 0xFF;
-            let st = format!("{}{}{}",red,green,blue);
+            let st = format!("{}{}{}", red, green, blue);
             if st.as_str() == "000000" {
-                pixel[0] = {let r2 = vec![self.area[pos],self.area[pos + 1]];
-                            let red2 = std::str::from_utf8(&r2).unwrap();
-                            u8::from_str_radix(red2,16).unwrap()};
-                pixel[1] = {let b2 = vec![self.area[pos+2],self.area[pos + 3]];
-                            let blu2 = std::str::from_utf8(&b2).unwrap();
-                            u8::from_str_radix(blu2,16).unwrap()};
-                pixel[2] = {let g2 = vec![self.area[pos+2],self.area[pos + 3]];
-                            let gre2 = std::str::from_utf8(&g2).unwrap();
-                            u8::from_str_radix(gre2,16).unwrap()};
+                pixel[0] = {
+                    let r2 = vec![self.area[pos], self.area[pos + 1]];
+                    let red2 = std::str::from_utf8(&r2).unwrap();
+                    u8::from_str_radix(red2, 16).unwrap()
+                };
+                pixel[1] = {
+                    let b2 = vec![self.area[pos + 2], self.area[pos + 3]];
+                    let blu2 = std::str::from_utf8(&b2).unwrap();
+                    u8::from_str_radix(blu2, 16).unwrap()
+                };
+                pixel[2] = {
+                    let g2 = vec![self.area[pos + 2], self.area[pos + 3]];
+                    let gre2 = std::str::from_utf8(&g2).unwrap();
+                    u8::from_str_radix(gre2, 16).unwrap()
+                };
             }
         }
     }
