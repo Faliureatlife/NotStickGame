a4f5ef2c217299b9a1258e57e6462c94bd024000
bob <mnick900@gmail.com>
2023-01-06 22:30:18 -0800

    artifacts gone, code formatted

--- math_game/src/main.rs
+++ math_game/src/main.rs
@@ -49,9 +49,9 @@ fn main() -> Result<(), pixels::Error> {
 
     //frame buffer "pixels"
     let mut pixels = Pixels::new(720, 540, surface_texture)?;
-    // for pixel in pixels.get_frame().chunks_exact_mut(4) {
-    //     pixel[3] = 255;
-    // }
+    for pixel in pixels.get_frame().chunks_exact_mut(4) {
+        pixel[3] = 255;
+    }
     //screen object that has the text.txt source file
     let mut screen = Screen::new("WorldData/dots.txt");
     //loop that runs program
@@ -99,7 +99,6 @@ fn main() -> Result<(), pixels::Error> {
             }
             //after updates happen redraw the screen
             window.request_redraw();
-
         };
     });
     //Ok(())
@@ -110,7 +109,7 @@ struct Player {
     //top right of player
     x_pos: u16,
     y_pos: u16,
-    move_state:u8,
+    move_state: u8,
     // data:
     sprite: Vec<u8>,
     //direction: u8
@@ -134,25 +133,37 @@ impl Player {
     }
     fn mov(&mut self, dir: u8) {
         match dir {
+            //TODO: make the movement flush with edges
+            //TODO: use different sprites for movement
             //Move up W
-            1 if self.y_pos > 3 => {self.y_pos -= 2;
-                                    self.move_state +=1;},
+            1 if self.y_pos > 3 => {
+                self.y_pos -= 2;
+                self.move_state += 1;
+            }
             1 => {}
             //Move left A
-            2 if self.x_pos > 1 => {self.x_pos -= 2;
-                                    self.move_state +=1;},
+            2 if self.x_pos > 1 => {
+                self.x_pos -= 2;
+                self.move_state += 1;
+            }
             2 => {}
             //Move down S
-            3 if self.y_pos < 511 => {self.y_pos += 2;
-                                    self.move_state +=1;},
+            3 if self.y_pos < 511 => {
+                self.y_pos += 2;
+                self.move_state += 1;
+            }
             3 => {}
             //Move right D
-            4 if self.x_pos < 700 => {self.x_pos += 2;
-                                    self.move_state +=1;},
+            4 if self.x_pos < 700 => {
+                self.x_pos += 2;
+                self.move_state += 1;
+            }
             4 => {}
             _ => panic!("Invalid movement"),
         }
-        if self.move_state == 4 {self.move_state -= 4;}
+        if self.move_state == 4 {
+            self.move_state -= 4;
+        }
     }
 }
 
@@ -165,82 +176,72 @@ struct Screen {
 }
 
 impl Screen {
-   fn new(place: &str) -> Self {
-       Self {
-           player: Player::new("SpriteData/Nav/up/back_nav0.txt"),
-           //baddies: vec![],
-           //check the types that are used if errors, maybe &str ?
-		   //area maybe as an array, convert to slice later on?
-           area: Screen::new_screen(place),
-
-       // add in the whole framebuffer thing and just copy it later
-       // render adds in just the characters on top
-       // i hate myself
-	   //render all of place into a vec<u8> same as pixels
-       // }
-       }
-   }
+    fn new(place: &str) -> Self {
+        Self {
+            player: Player::new("SpriteData/Nav/up/back_nav0.txt"),
+            //baddies: vec![],
+            //check the types that are used if errors, maybe &str ?
+            //area maybe as an array, convert to slice later on?
+            area: Screen::new_screen(place),
+            // add in the whole framebuffer thing and just copy it later
+            // render adds in just the characters on top
+            // i hate myself
+            //render all of place into a vec<u8> same as pixels
+            // }
+        }
+    }
     fn new_screen(place: &str) -> Vec<u8> {
         // let mut bit:bool = false;
         let mut data = vec![];
         // let mut real_data: Vec<u8> = vec![];
-        for pix in std::fs::read(place).unwrap().chunks_exact(2){
+        for pix in std::fs::read(place).unwrap().chunks_exact(2) {
             // bit = !bit;
             //std::str::from_utf8(&g).unwrap()
             //u8::from_str_radix(blu2, 16).unwrap()
             //gives a vec<u8> of all "valid" bits for the fb without the added opacity bits
-            data.push(u8::from_str_radix(std::str::from_utf8(pix).unwrap(),16).unwrap());
+            data.push(u8::from_str_radix(std::str::from_utf8(pix).unwrap(), 16).unwrap());
             // write!(a,"{:.?}", "{b}")
         }
-        // println!("Its this long, supposed to be like 2,332,800: {}",data.len());
-        // for (it,x) in data.into_iter().enumerate() {
-        //     // if it == 0 {continue;}
-        //     if it % 3 == 0 {
-        //         real_data.push(255);
-        //     }
-        //     real_data.push(x);
-        // }
-        // real_data.remove(0);
-        // let a = format!("{:?}",&real_data);
-        // std::fs::write("without_opacity.txt", a).unwrap();
-        // println!("File created");
-        //output the whole thing
         data
     }
-//pix never used but needed in order to draw to framebuffer
+    //pix never used but needed in order to draw to framebuffer
     fn draw(&self, pix: &mut [u8]) {
-    let mut it2 = 0;
-    for (it,pixel) in pix.chunks_exact_mut(4).enumerate() {
-            if it % 720 > self.player.x_pos as usize && it % 720 < (self.player.x_pos + 19) as usize {
-                //it % 720 = x
-                if it / 720 > self.player.y_pos as usize && it / 720 < (self.player.y_pos + 28) as usize{
-                    // it / 720 = y
-                    // println!("char {}",it);
-                    pixel[0] = self.player.sprite[(it2)*3];
-                    //do the expect thing tomorrow
-                    pixel[1] = self.player.sprite[(it2)*3 + 1];
-                    pixel[2] = self.player.sprite[(it2)*3 + 2];
-                    pixel[3] = 255;
-                    it2 += 1;
-                }
+        //TODO: Update in chunks
+        //TODO: Use premade transparency values
+        let mut it2: usize = 0;
+        for (it, pixel) in pix.chunks_exact_mut(4).enumerate() {
+            /*Four checks:
+            it % 720 > x_pos
+            it % 720 < x_pos + 19
+            it / 720 > y_pos
+            it / 720 < y_pos + 28
+            */
+            if it % 720 > self.player.x_pos as usize
+                && it % 720 < (self.player.x_pos + 19) as usize
+                && it / 720 > self.player.y_pos as usize
+                && it / 720 < (self.player.y_pos + 28) as usize
+            {
+                // println!("char {}",it);
+                pixel[0] = self.player.sprite[(it2) * 3];
+                //do the expect thing tomorrow
+                pixel[1] = self.player.sprite[(it2) * 3 + 1];
+                pixel[2] = self.player.sprite[(it2) * 3 + 2];
+                // pixel[3] = 255;
+                it2 += 1;
             } else {
                 // println!("b {}",it);
                 pixel[0] = self.area[it * 3];
                 pixel[1] = self.area[it * 3 + 1];
                 pixel[2] = self.area[it * 3 + 2];
-                pixel[3] = 255;
+                // pixel[3] = 255;
             }
-
         }
 
-
-
-    //0-388799 it, should be right amt
-    //testing the fb contents
-    // let a = format!("{:?}",&pix);
-    // std::fs::write("framebuffer.txt", a).unwrap();
-    // println!("File created");
-
+        //0-388799 it, should be right amt
+        //testing the fb contents
+        // let a = format!("{:?}",&pix);
+        // std::fs::write("framebuffer.txt", a).unwrap();
+        // println!("File created");
     }
 }
 // fn _update(&mut self, sc) -> std::io::Result <()> {
