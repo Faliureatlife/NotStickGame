fd26ebb66c6f24da79e46ee3e5469cbfa4633d48
bob <mnick900@gmail.com>
2023-02-09 00:29:03 -0800

    now says done when completed

--- colors.py
+++ colors.py
@@ -90,7 +90,7 @@ def rgb_to_hex(red, green, blue):
 
 
 # Loads Image
-img = Image.open("math_game/SpriteData/Nav/down/pixil-frame-2.png")
+img = Image.open("math_game/SpriteData/Nav/up/pixil-frame-1.png")
 width, height = img.size
 pixels = img.load()
 allColors = ""
@@ -103,8 +103,9 @@ for y in range(height):
         allColors = allColors + rgb_to_hex(r, g, b)
 
 # open text file
-text_file = open("math_game/SpriteData/Nav/down/2.txt", "w+")
+text_file = open("math_game/SpriteData/Nav/up/0.txt", "w+")
 # write string to file
 text_file.write(allColors)
+print("done")
 # close file
 text_file.close()
